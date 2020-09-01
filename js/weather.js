76623dced64f397755feeb0d276c5a4292ad738e
Léo Martin <leo.martin5@etu.univ-lorraine.fr>
2020-09-01 08:07:39 +0200

    Paris lat/long

--- js/weather.js
+++ js/weather.js
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 48.8534;
+    let longitude = 2.3488;
 
     getWeather(latitude, longitude);
 }
