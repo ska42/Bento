49e19d83b2d39c64914131b9b526140e1b878fcd
Léo Martin <leo.martin5@etu.univ-lorraine.fr>
2020-09-01 08:04:08 +0200

    weather_api key

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = 'cff7408bb3f95148a3d701ac451627f0';
 
 // Set Position function
 setPosition();
